82bdfb60ab713a135c8df0aaace82a2c77f09e48
Rawan-Eltobgy <rawaneltobgy@gmail.com>
2020-09-21 00:41:02 +0200

    Add HomeScreen searchBar (UI)

---
+++ src/config/styles.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: '#B50717',
+  secondary: '#131313', // Header background
+  disabled: '#dadee0',
+  white: '#FFFFFF',
+  black: '#000000',
+  yellow: '#FFBD19',
+  white1: '#F8F8F8',
+  darkGrey: '#424242',
+  error: '#ff190c',
+  warn: '#606060',
+  tabsTextColor: '#a4a4a4',
+  lightShadow: '#F5F5F5',
+};
---
+++ src/screens/homeScreen/HomeScreen.tsx
@@ -0,0 +1,94 @@
+import React, {useState} from 'react';
+import {TextField, View} from 'react-native-ui-lib';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from '@types/react-native-vector-icons';
+
+import {colors} from '../../config/styles';
+
+function HomeScreen() {
+  const [movieName, setMovieName] = useState('');
+
+  const onSearch = (event: {
+    nativeEvent: {text: React.SetStateAction<string>};
+  }) => {
+    setMovieName(event.nativeEvent.text);
+    // {
+    //   movieName && this.props.search({
+    //     'name': movieName,
+    //   });
+    // }
+  };
+
+  return (
+    <View flex padding-24>
+      <TouchableOpacity
+        style={[styles.searchView, styles.shadowInput, styles.roundedInput]}>
+        <TextField
+          style={styles.inputStyle}
+          placeholder="Search movies, series, or any title in mind !"
+          underlineColorAndroid="transparent"
+          enableErrors={false}
+          hideUnderline
+          placeholderTextColor="#bbb"
+          onChange={(event: any) => {
+            onSearch(event);
+          }}
+          returnKeyType="search"
+          // onSubmitEditing={}
+          clearButtonMode="while-editing"
+        />
+        {/* <Icon
+          containerStyle={styles.itemIcon}
+          color={colors.yellow}
+          size={22}
+          name="search"
+          type="evilIcons"
+        /> */}
+      </TouchableOpacity>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  searchView: {
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    borderRadius: 20,
+    borderColor: colors.darkGrey,
+    height: 45,
+  },
+  shadowInput: {
+    height: 44,
+    backgroundColor: '#ffffff',
+    shadowColor: 'rgba(0, 0, 0, 0.07)',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowRadius: 30,
+    shadowOpacity: 1,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#ffffff',
+    elevation: 2,
+  },
+  roundedInput: {
+    borderRadius: 35,
+  },
+  inputStyle: {
+    height: 45,
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    color: colors.secondary,
+    fontSize: 13,
+    lineHeight: undefined,
+    textAlignVertical: 'center',
+  },
+  itemIcon: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 15,
+  },
+});
+
+export default HomeScreen;
